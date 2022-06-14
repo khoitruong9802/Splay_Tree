de56c9fbf6afbf20f483bcc9e0b1fff1856532d6
Trương Mạnh Khôi <khoitruong9802@gmail.com>
2022-06-14 21:44:40 +0700

    Add interpolationSearch file (Right Version)

--- interpolationSearch.cpp
+++ interpolationSearch.cpp
@@ -4,20 +4,35 @@ using namespace std;
 
 int interpolationSearch(int arr[], int left, int right, int x)
 {
-    if (left == right) {
-        return -1;
+    // if (left == right) {
+    //     return -1;
+    // }
+    // int index = left + (x - arr[left]) * (right - left) / (arr[right] - arr[left]);
+    // if (index > right || index < left) {
+    //     return -1;
+    // }
+    // if (arr[index] == x) {
+    //     return index;
+    // } 
+    // cout << "We traverse on index: " << index << endl;
+    // if (arr[index] > x) {
+    //     return interpolationSearch(arr, left, index - 1, x);
+    // } else if (arr[index] < x) {
+    //     return interpolationSearch(arr, index + 1, right, x);
+    // }
+    if (left <= right && x >= arr[left] && x <= arr[right]) {   
+        int pos = left + (x - arr[left]) * (right - left) / (arr[right] - arr[left]);
+        cout << "We traverse on index: " << pos << endl;
+        if (arr[pos] == x)
+            return pos;
+        if (arr[pos] < x) {
+            return interpolationSearch(arr, pos + 1, right, x);
+        }
+        if (arr[pos] > x) {
+            return interpolationSearch(arr, left, pos - 1, x);   
+        }
     }
-    int index = left + (x - arr[left]) * (right - left) / (arr[right] - arr[left]);
-    if (index > right || index < left) {
-        return -1;
-    }
-    cout << "We traverse on index: " << index << endl;
-    if (arr[index] > x) {
-        return interpolationSearch(arr, left, index - 1, x);
-    } else if (arr[index] < x) {
-        return interpolationSearch(arr, index + 1, right, x);
-    }
-    return index;
+    return -1;
 }
 
 int main() {
